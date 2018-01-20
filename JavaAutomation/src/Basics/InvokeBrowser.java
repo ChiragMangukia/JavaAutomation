86914412f5ff240c224d1bb3940741e3efd5d947
Mansi <Mansi@192.168.0.6>
2018-01-21 00:28:04 +0530

    First commit.

---
+++ JavaAutomation/src/Basics/InvokeBrowser.java
@@ -0,0 +1,23 @@
+package Basics;
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InvokeBrowser {
+
+	public static void main(String[] args) {
+		
+		File filePath = new File("BrowserDrivers", "chromedriver.exe");
+		
+		System.setProperty("webdriver.chrome.driver", filePath.getAbsolutePath());
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("chiragmangukia");
+		//driver.close();
+	}
+
+}
