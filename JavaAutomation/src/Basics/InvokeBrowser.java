d176caee539c5a8623f43ee411fd119569590d8e
chirag.mangukiya <chirag.mangukiya@si-chirag.LGE.NET>
2018-01-22 13:56:09 +0530

    Added Thread Sleep before closing the browser.

--- JavaAutomation/src/Basics/InvokeBrowser.java
+++ JavaAutomation/src/Basics/InvokeBrowser.java
@@ -1,12 +1,14 @@
 package Basics;
 import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InvokeBrowser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		File filePath = new File("BrowserDrivers", "chromedriver.exe");
 		
@@ -18,7 +20,9 @@ public class InvokeBrowser {
 		driver.getTitle();
 		System.out.println(driver.getTitle());
 		driver.findElement(By.id("email")).sendKeys("chiragmangukia");
-		//driver.close();
+		//driver.manage().timeouts().implicitlyWait(15000, TimeUnit.SECONDS);
+		Thread.sleep(10000);
+		driver.close();
 	}
 
 }
