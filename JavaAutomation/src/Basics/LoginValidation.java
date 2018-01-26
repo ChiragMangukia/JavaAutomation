d5083975ae59df7819aa4e20fbc4809640e8f80b
ChiragMangukia <chirag.mangukia@gmail.com>
2018-01-26 20:53:55 +0530

    Added all the logical codes in separate Method and called in Main method with try catch block.

--- JavaAutomation/src/Basics/LoginValidation.java
+++ JavaAutomation/src/Basics/LoginValidation.java
@@ -1,36 +1,39 @@
 package Basics;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginValidation {
+	
+	private static WebDriver driver = myMethods.Driver();
 
 	public static void main(String[] args) throws IOException {
-				
-		WebDriver driver = myMethods.Driver();
+		try {
+			loginValidation();
+		} finally {
+			driver.close();
+			myMethods.killProcess();
+		}
+	}
+	
+	private static void loginValidation() {
 		
 		String tarUrl = "https://login.salesforce.com/";
 		driver.get(tarUrl);
-		driver.findElement(By.xpath("//*[@id=\'username\']")).sendKeys("chiragmangukia");
-		driver.findElement(By.xpath("//*[@id=\'password\']")).sendKeys("password");
-		driver.findElement(By.xpath("//*[@id=\'Login\']")).click();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("chiragmangukia");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("mypassword");
+		driver.findElement(By.cssSelector("#Login")).click();
 		
 		String curUrl = driver.getCurrentUrl();
 		if(tarUrl.equals(curUrl)) {
 			String errText = driver.findElement(By.cssSelector("#error")).getText();
 			System.out.println(errText);			
-		}
-		else {
+		} else {
 			System.out.println("Login Success");
 		}
 		
-		driver.close();
-		myMethods.killProcess();
-
 	}
 
 }
