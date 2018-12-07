995b03cb1f0fb5feb8c6df042c6ecc63b4661fbb
Chirag Mangukiya/LGSIA PVG SW APPROVAL-T1(chirag.mangukiya@lge.com) <chirag.mangukiya@lge.com>
2018-12-07 15:55:19 +0530

    Misc changes

---
+++ JavaAutomation/src/Basics/test.java
@@ -0,0 +1,31 @@
+package Basics;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class test {
+
+	public static void main(String[] args) throws IOException {
+		
+		String testFile = "D:\\test.txt";
+		//File FC = new File(testFile);//Created object of java File class.
+		//FC.createNewFile();
+		
+		
+		FileReader FR = new FileReader(testFile);
+		BufferedReader BR = new BufferedReader(FR);
+		String Content = "";
+		  
+		//Loop to read all lines one by one from file and print It.
+		while((Content = BR.readLine())!= null){
+		System.out.println(Content);
+		}
+		FR.close();
+
+	}
+	
+}
