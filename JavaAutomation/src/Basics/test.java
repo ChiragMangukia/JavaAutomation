122924cd615247fdb459be44ebf93652e4b82af7
chiragmangukia <chirag.mangukia@gmail.com>
2018-12-18 08:19:06 +0530

    Misc

--- JavaAutomation/src/Basics/test.java
+++ JavaAutomation/src/Basics/test.java
@@ -1,10 +1,7 @@
 package Basics;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class test {
