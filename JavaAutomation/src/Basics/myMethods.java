bb49d3421197b10b1bf85d8de1fb3524860239f5
chiragmangukia <chirag.mangukia@gmail.com>
2018-12-18 11:09:13 +0530

    Misc

--- JavaAutomation/src/Basics/myMethods.java
+++ JavaAutomation/src/Basics/myMethods.java
@@ -3,7 +3,7 @@ package Basics;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
+//import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -12,16 +12,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class myMethods {
 	
-	public void Screenshot(WebDriver webElement, String pathName) throws IOException {		
-		File file = ((TakesScreenshot)webElement).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(file, new File(pathName));		
-	}
+	//public void Screenshot(WebDriver webElement, String pathName) throws IOException {		
+	//	File file = ((TakesScreenshot)webElement).getScreenshotAs(OutputType.FILE);
+	//	FileUtils.copyFile(file, new File(pathName));		
+	//}
 	
 	public static WebDriver Driver() {
 		File filePath = new File("BrowserDrivers", "chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", filePath.getPath());
 		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setHeadless(true);
+		chromeOptions.setHeadless(false);
 		WebDriver driver = new ChromeDriver(chromeOptions);
 		return driver;
 	}
